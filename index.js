48bf2b3e9cbe44089ea479f3bc8c4c9932fd0ed1
sohan-r30 <sohanakondo139@gmail.com>
2023-06-08 11:29:08 +0600

    store user data in mongodb

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 2007
 
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200,
-  }
-  app.use(cors(corsOptions))
-  app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+
+  const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+      return res.status(401).send({ error: "Unauthorized access!" });
+    }
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+      if (error) {
+        return res.status(403).send({ error: "Unauthorized access!" });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  }
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fhpe21r.mongodb.net/?retryWrites=true&w=majority`;
@@ -31,13 +43,29 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("summerCampDB").collection("users");
 
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5h' })
+      res.send({ token })
+    })
 
-
-    
-
-
+    // Users api
+      app.put('/users/:email', async (req, res) => {
+        const email = req.params.email
+        const user = req.body
+        const query = { email: email }
+        const options = { upsert: true }
+        const updateDoc = {
+          $set: {...user},
+        }
+        const result = await usersCollection.updateOne(query, updateDoc, options)
+        res.send(result)
+      })
+      
+      
 
 
     // Send a ping to confirm a successful connection
